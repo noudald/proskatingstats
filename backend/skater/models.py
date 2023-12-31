e886066999ad6037bffd4c4bd23cfed53369c715
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-12-31 06:41:20 +0100

    Add skater model

--- backend/skater/models.py
+++ backend/skater/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+COUNTRIES = (
+    ('NL', 'Netherlands'),
+)
+
+GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+
+class Skater(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    country = models.CharField(max_length=2, choices=COUNTRIES)
+    birthday = models.DateField()
