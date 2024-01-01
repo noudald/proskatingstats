7b0b5ce967750bc58c1c9d96eef54afc54afeb8d
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2024-01-01 07:08:46 +0100

    Register all models to admin panel

--- backend/skater/admin.py
+++ backend/skater/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Skater,
+    Track,
+    Race,
+    Distance,
+    RaceResult,
+)
+
+
+admin.site.register(Skater)
+admin.site.register(Track)
+admin.site.register(Race)
+admin.site.register(Distance)
+admin.site.register(RaceResult)
